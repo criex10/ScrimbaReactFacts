44060573f35f260668570ccafe4d2cd26aa0d967
criex10 <criex10@gmail.com>
2023-10-13 22:00:18 -0700

    Add in Hello World style components
    This removes a lot of boilerplate and puts in the basics of
    a website for scrimba.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ["400", "600", "700"]
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
