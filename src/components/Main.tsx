37ac96a36347e801eedddb4498401faff5c6ead7
criex10 <criex10@gmail.com>
2023-10-14 22:12:03 -0700

    Add styling to the Main component
    This also adds in some extra global styling for the
    whole page to come together.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,24 @@
+import styles from "./Main.module.css";
+
+function Fact(props: { message: string }) {
+    return (
+        <li className={styles.listItem}>
+            <span className={styles.listText}>{props.message}</span>
+        </li>
+    );
+}
+
 export function Main() {
-    return <div>Main component</div>;
+    return (
+        <div className={styles.main}>
+            <h1 className={styles.header}>Fun facts about React</h1>
+            <ul>
+                <Fact message={'Was first released in 2013'}/>
+                <Fact message={'Was originally created by Jordan Walke'}/>
+                <Fact message={'Has well over 100k stars on GitHub'}/>
+                <Fact message={'Is maintained by Facebook'}/>
+                <Fact message={'Powers thousands of enterprise apps, including mobile apps'}/>
+            </ul>
+        </div>
+    );
 }
